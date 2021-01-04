d377a2e5cce8c0205a97946a318eec69b4d71f1c
Andrew Huth <andrew@huth.me>
2021-01-03 22:24:24 -0800

    Use parallel states

--- src/state-machines.ts
+++ src/state-machines.ts
@@ -1,39 +1,45 @@
 import { Machine } from 'xstate';
 
-export const grinder = Machine({
-  id: 'grinder',
-  initial: 'empty',
+export const coffee = Machine({
+  id: 'coffee',
+  type: 'parallel',
   states: {
-    empty: {
-      on: {
-        FILL: 'full',
-        TURN_ON: 'running_empty',
-      },
-    },
-    running_empty: {
-      after: {
-        2000: 'empty',
-      },
-    },
-    full: {
-      on: {
-        TURN_ON: 'running',
-      },
-    },
-    running: {
-      after: {
-        2000: 'ground',
-      },
-    },
-    ground: {
-      on: {
-        EMPTY: 'empty',
-        TURN_ON: 'running_ground',
-      },
-    },
-    running_ground: {
-      after: {
-        2000: 'ground',
+    grinder: {
+      id: 'grinder',
+      initial: 'empty',
+      states: {
+        empty: {
+          on: {
+            FILL: 'full',
+            TURN_ON: 'running_empty',
+          },
+        },
+        running_empty: {
+          after: {
+            2000: 'empty',
+          },
+        },
+        full: {
+          on: {
+            TURN_ON: 'running',
+          },
+        },
+        running: {
+          after: {
+            2000: 'ground',
+          },
+        },
+        ground: {
+          on: {
+            EMPTY: 'empty',
+            TURN_ON: 'running_ground',
+          },
+        },
+        running_ground: {
+          after: {
+            2000: 'ground',
+          },
+        },
       },
     },
   },
