cf3f9e250c6f61b200aaca872c15ef0c3a05e6ec
Andrew Huth <andrew@huth.me>
2021-01-03 21:49:44 -0800

    Add statechart for the grinder

---
+++ src/state-machines.ts
@@ -0,0 +1,40 @@
+import { Machine } from 'xstate';
+
+export const grinder = Machine({
+  id: 'grinder',
+  initial: 'empty',
+  states: {
+    empty: {
+      on: {
+        FILL: 'full',
+        TURN_ON: 'running_empty',
+      },
+    },
+    running_empty: {
+      after: {
+        2000: 'empty',
+      },
+    },
+    full: {
+      on: {
+        TURN_ON: 'running',
+      },
+    },
+    running: {
+      after: {
+        2000: 'ground',
+      },
+    },
+    ground: {
+      on: {
+        EMPTY: 'empty',
+        TURN_ON: 'running_ground',
+      },
+    },
+    running_ground: {
+      after: {
+        2000: 'ground',
+      },
+    },
+  },
+});
