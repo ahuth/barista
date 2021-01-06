aa50de5e97f477f0aa69d33be6f0ab3e42929a6c
Andrew Huth <andrew@huth.me>
2021-01-05 20:32:43 -0800

    Revert "Use parallel states"
    This reverts commit d377a2e5cce8c0205a97946a318eec69b4d71f1c.

--- src/state-machines.ts
+++ src/state-machines.ts
@@ -1,45 +1,39 @@
 import { Machine } from 'xstate';
 
-export const coffee = Machine({
-  id: 'coffee',
-  type: 'parallel',
+export const grinder = Machine({
+  id: 'grinder',
+  initial: 'empty',
   states: {
-    grinder: {
-      id: 'grinder',
-      initial: 'empty',
-      states: {
-        empty: {
-          on: {
-            FILL: 'full',
-            TURN_ON: 'running_empty',
-          },
-        },
-        running_empty: {
-          after: {
-            2000: 'empty',
-          },
-        },
-        full: {
-          on: {
-            TURN_ON: 'running',
-          },
-        },
-        running: {
-          after: {
-            2000: 'ground',
-          },
-        },
-        ground: {
-          on: {
-            EMPTY: 'empty',
-            TURN_ON: 'running_ground',
-          },
-        },
-        running_ground: {
-          after: {
-            2000: 'ground',
-          },
-        },
+    empty: {
+      on: {
+        FILL: 'full',
+        TURN_ON: 'running_empty',
+      },
+    },
+    running_empty: {
+      after: {
+        2000: 'empty',
+      },
+    },
+    full: {
+      on: {
+        TURN_ON: 'running',
+      },
+    },
+    running: {
+      after: {
+        2000: 'ground',
+      },
+    },
+    ground: {
+      on: {
+        EMPTY: 'empty',
+        TURN_ON: 'running_ground',
+      },
+    },
+    running_ground: {
+      after: {
+        2000: 'ground',
       },
     },
   },
